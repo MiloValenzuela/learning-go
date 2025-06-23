69e033a27d73f7e52b2feeeb672df77889b98edb
MiloValenzuela <valenzuelagarza@gmail.com>
2025-06-23 17:41:16 -0600

    Types and Structs

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+var s = "seven"
+
+// var firstName string
+// var lastName string
+// var phoneNumber string
+// var age int
+// var birthDate time.Time
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
 
 func main() {
-	fmt.Println("hello")
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "1 555-555-1212",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
 }
