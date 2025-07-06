2c9180514c8d4c53e94ab9827054d3da9bd639ec
MiloValenzuela <valenzuelagarza@gmail.com>
2025-07-06 16:03:06 -0600

    Decision Structures

--- 2-hello-world-web/main.go
+++ 2-hello-world-web/main.go
@@ -3,7 +3,19 @@ package main
 import "log"
 
 func main() {
-	names := []string{"one", "seven", "fish", "cat"}
 
-	log.Println(names)
+	myVar := "fish"
+
+	switch myVar {
+	case "cat":
+		log.Println("Cat is set to cat")
+	case "dog":
+		log.Println("cat is set to dog")
+	case "fish":
+		log.Println("cat is set to fish")
+
+	default:
+		log.Println("cat is something else")
+	}
+
 }
